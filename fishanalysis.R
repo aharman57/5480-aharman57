986a1d442950c745411811a817a7560e96135b3c
aharman57 <harman@zoology.ubc.ca>
2021-02-10 15:58:56 -0800

    new comment

--- fishanalysis.R
+++ fishanalysis.R
@@ -1,5 +1,7 @@
+#reading in .csv data
 fish_data <- read.csv(file="Gaeta_etal_CLC_data.csv", header = T)
 
 library(dplyr)
+
 fish_data_cat = fish_data %>% 
   mutate(length_cat = ifelse(length > 300, "big", "small"))
