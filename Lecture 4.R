8524b1da477c612b7825493bbe04648ee65da8b5
aharman57 <harman@zoology.ubc.ca>
2021-02-23 16:03:10 -0800

    Exercise 3

--- Lecture 4.R
+++ Lecture 4.R
@@ -10,6 +10,9 @@ mutate(surveys, hindfoot_length_cm = hindfoot_length / 10)
 
 surveys_plus <- mutate(surveys,
                        hindfoot_length_cm = hindfoot_length / 10)
+
+### EXERCISE 1 ###
+
 #1-2
 shrub <- read_csv("data/shrub-volume-data.csv")
 names(shrub)
@@ -38,3 +41,65 @@ summarize(surveys_by_species_plot, abundance = n())
 
 species_weight <- summarize(surveys_by_species, avg_weight = mean(weight))
 na.omit(species_weight)
+
+ds_data <- filter(surveys, species_id == "DS")
+ds_data_by_year <- group_by(ds_data, year)
+ds_weight_by_year <- summarize(ds_data_by_year,
+                               avg_weight = mean(weight, na.rm = TRUE))
+
+### EXERCISE 2 ###
+
+#1
+surveys_new_1 <- select(surveys, year, month, day, species_id)
+
+#2
+surveys_new_2 <- select(surveys, year, species_id, weight)
+surveys_new_2 <- mutate(surveys_new_2, weight = weight/1000)
+surveys_new_2 <- na.omit(surveys_new_2)
+
+#3
+surveys_new_3 <- filter(surveys, species_id == "SH")
+
+#4
+surveys_new_4 <- group_by(surveys, species_id)
+summarize(surveys_new_4, abundance = n())
+
+#5
+surveys_new_5 <- group_by(surveys, species_id, year)
+summarize(surveys_new_5, abundance = n())
+
+#6
+surveys_data <- filter(surveys, species_id == "DO")
+surveys_new_6 <- group_by(surveys_data, year)
+summarize(surveys_new_6, avg_weight = mean(weight, na.rm = TRUE))
+
+### Exercise 3 ###
+
+#1 
+surveys_pipe_1 <- surveys %>%
+  select(year, species_id, weight) %>%
+  mutate(weight = weight/1000) %>%
+  na.omit()
+
+#2
+surveys_pipe_2 <- surveys %>%
+  filter(species_id == "SH") %>%
+  select(year, month, day, species_id)
+
+#3
+surveys_pipe_3 <- surveys %>%
+  group_by(species_id) %>%
+  summarize(abundance = n())
+
+#4
+surveys_pipe_4 <- surveys %>%
+  group_by(species_id, year) %>%
+  summarize(abundance = n())
+
+#5
+surveys_pipe_5 <- surveys %>%
+  filter(species_id == "DO") %>%
+  group_by(year) %>%
+  summarize(avg_weight = mean(weight, na.rm = T))
+
+view(surveys_pipe_5)
